f0f16a8d91c5b3da559366a086101ff7fda3530a
Michele Riva <ciao@micheleriva.it>
2022-08-06 17:44:35 +0200

    feat: adds metadata

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,15 +1,39 @@
 import type { AppProps } from 'next/app'
 import { NavBar } from '../components/NavBar'
-import { Footer } from '../components/Footer';
+import { Footer } from '../components/Footer'
+import { DefaultSeo } from 'next-seo'
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <div className='w-full min-h-screen bg-slate-100 text-slate-100'>
-      <NavBar />
-      <Component {...pageProps} />
-      <Footer />
-    </div>
+    <>
+      <DefaultSeo
+        title='✨ Lyra'
+        description='Fast, in-memory, typo-tolerant, full-text search engine written in TypeScript'
+        defaultTitle='✨ Lyra'
+        openGraph={{
+          type: 'website',
+          locale: 'en_IE',
+          url: 'https://lyrajs.io/',
+          site_name: 'Lyra',
+          description: 'Fast, in-memory, typo-tolerant, full-text search engine written in TypeScript',
+          title: 'Lyra',
+          images: [
+            {
+              url: 'https://lyrajs.io/imgs/seo/lyra-og-image.png',
+              alt: 'Lyra, full-text search engine',
+              height: 1080,
+              width: 1920,
+            }
+          ]
+        }}
+      />
+      <div className='w-full min-h-screen bg-slate-100 text-slate-100'>
+        <NavBar />
+        <Component {...pageProps} />
+        <Footer />
+      </div>
+    </>
   );
 }
 
